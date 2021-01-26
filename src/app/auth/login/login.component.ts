d7c37226ae49acb2689ac3a90bdb098af6e1f074
James Volmert <james.volmert@gmail.com>
2021-01-25 19:50:15 -0700

    fixed the bugs with login/signup

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  passwordReduired = "Password is required"
   constructor() { }
 
   ngOnInit(): void {
