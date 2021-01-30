50b079ab3f0c3ffbcb02aac675988567cbb7294a
James Volmert <james.volmert@gmail.com>
2021-01-30 15:06:52 -0700

    fixed typos with welcome page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  activity = "ACTIVITY";
+  activityQuote = "Stay active and enjoy better health and more fun!";
+  community = "COMMUNITY";
+  communityQuote = "Get to know more people and share your passion";
+  challenge = "CHALLENGE";
+  challengeQuote = "Never stop challenging yourself everyday";
+  
   constructor() { }
 
   ngOnInit(): void {
